ba15097e38e17c74b7d358262c829bf0204e976e
Cristofer Ledesma <ledes7080@gmail.com>
2023-10-17 12:51:57 -0400

    fix the close button

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,60 @@
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// module.exports = {
-//   devtool: "inline-source-map",
-//   entry: {
-//     main: "./src/index.js",
-//   },
-//   stats: "errors-only",
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "main.js",
-//     publicPath: "",
-//   },
-//   mode: "development",
-//   devserver: {
-//     static: path.resolve(__dirname, "./dist"),
-//     compress: true,
-//     port: 8080,
-//     open: true,
-//     liveReload: true,
-//     hot: false,
-//   },
-//   // module: {
-//   //   rule: [
-//   //     {
-//   //       test: /\.js$/,
-//   //       loader: "babel-loader",
-//   //       exclude: "/node_modules/",
-//   //     },
-//   //     {
-//   //       test: /\.css$/,
-//   //       use: [
-//   //         MiniCssExtractPlugin.loader,
-//   //         {
-//   //           loader: "css-loader",
-//   //           options: { importLoaders: 1 },
-//   //         },
-//   //         "postcss-loader",
-//   //       ],
-//   //     },
-//   //     {
-//   //       // add the rule for processing files
-//   //       test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
-//   //       type: "asset/resource",
-//   //     },
-//   //   ],
-//   // },
+module.exports = {
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/index.js",
+  },
+  stats: "errors-only",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  mode: "development",
+  devserver: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    liveReload: true,
+    hot: false,
+  },
+  module: {
+    rule: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        // add the rule for processing files
+        test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
 
-//   stats: "errors-only",
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: "./src/index.html",
-//     }),
-//     new CleanWebpackPlugin(),
-//     new MiniCssExtractPlugin(),
-//   ],
-// };
+  stats: "errors-only",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
